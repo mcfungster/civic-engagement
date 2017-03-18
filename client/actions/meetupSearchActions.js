3977d0fa6964e471170c19213398b5303afa9ea6
Rishi Shah <shah.rishi05@gmail.com>
2017-03-18 14:52:50 -0400

    Revert change to meetupSearchAction

--- client/actions/meetupSearchActions.js
+++ client/actions/meetupSearchActions.js
@@ -1,11 +1,9 @@
 import Axios from 'axios';
 
 export default function (location) {
-  if (location) {
-    const request = Axios.get(`/api/meetup/${location}`);
-    return {
-      type: 'Meetup_Search',
-      payload: request
-    };
-  }
+  const request = Axios.get(`/api/meetup/${location}`);
+  return {
+    type: 'Meetup_Search',
+    payload: request
+  };
 }
