e5f08edf6d17fcc070ca2af1af54322fe13fce92
Rishi Shah <shah.rishi05@gmail.com>
2017-03-18 16:22:37 -0400

    Remove commented out code & add proptype validation to GoogleMapsContainer

--- client/containers/GoogleMapContainer.js
+++ client/containers/GoogleMapContainer.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { GoogleMapLoader, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 import { toggleInfoWindow, closeInfoWindow } from '../actions/meetupMapActions';
-// import sampleData from '../reference/markerDummyData';
-// import meetupData from '../reference/meetupDummyData';
 
 class MapContainer extends Component {
   constructor(props, context) {
@@ -15,13 +13,8 @@ class MapContainer extends Component {
         lng: -73.988249
       },
       defaultZoom: 9
-      // windowPosition: null,
-      // showInfoWindow: false,
-      // current_event: ''
     };
-    // this.toggleInfoWindow = this.toggleInfoWindow.bind(this);
   }
-
 
   componentDidUpdate(prevProps) {
     console.log('In Component Did Update');
@@ -31,28 +24,10 @@ class MapContainer extends Component {
     this._fitTheBounds();
   }
 
-  // toggleInfoWindow(event, loc) {
-  //   if (loc === null) {
-  //     this.setState({ windowPosition: null });
-  //     return;
-  //   }
-  //   const markerLoc = {
-  //     lat: loc.latLng.lat(),
-  //     lng: loc.latLng.lng()
-  //   };
-  //   this.setState({
-  //     current_event: event,
-  //     windowPosition: markerLoc,
-  //     showInfoWindow: true
-  //   });
-  // }
-
   _fitTheBounds() {
-    console.log('_fitTheBounds has been triggered');
     const bounds = new window.google.maps.LatLngBounds();
     const results = this.props.MeetupEvents;
     const map = this.map;
-
     results.forEach((event) => {
       let vLat = event.venue ? event.venue.lat || event.group.group_lat : event.group.group_lat;
       let vLng = event.venue ? event.venue.lon || event.group.group_lon : event.group.group_lon;
@@ -62,9 +37,7 @@ class MapContainer extends Component {
     });
     map.fitBounds(bounds);
     map.panToBounds(bounds);
-    // this._fitTheBounds = () => {};
     const currentZoom = map.getZoom();
-    console.log(currentZoom);
     if (currentZoom > 15) {
       map.setZoom(15);
       setTimeout(() => {
@@ -78,14 +51,13 @@ class MapContainer extends Component {
     let markers = [];
     if (results) {
       markers = results.map((event, index) => {
-        let vLat = event.venue ? event.venue.lat || event.group.group_lat : event.group.group_lat;
-        let vLng = event.venue ? event.venue.lon || event.group.group_lon : event.group.group_lon;
+        const vLat = event.venue ? event.venue.lat || event.group.group_lat : event.group.group_lat;
+        const vLng = event.venue ? event.venue.lon || event.group.group_lon : event.group.group_lon;
         return (
           <Marker
             position={{ lat: vLat, lng: vLng }}
             key={event.id}
             index={index}
-            // onClick={this.toggleInfoWindow.bind(this, event)}
             onClick={this.props.toggleInfoWindow.bind(this, event)}
           />
         );
@@ -105,20 +77,13 @@ class MapContainer extends Component {
             >
               { markers }
               {
-                // this.state.showInfoWindow &&
                 this.props.InfoWindow.showInfoWindow &&
                 <InfoWindow
-                  // position={this.state.windowPosition}
-                  // onCloseclick={() => { this.setState({ showInfoWindow: false }); }}
                   position={this.props.InfoWindow.windowPosition}
                   onCloseclick={this.props.closeInfoWindow}
                   options={{ pixelOffset: new window.google.maps.Size(0, -30) }}
                 >
                   {
-                    // `<h2>${this.state.current_event.name}</h2>
-                    // <h4>Hosted By: ${this.state.current_event.group.name}</h4>
-                    // <h4>When: ${new Date(this.state.current_event.time)}</h4>
-                    // <p>${this.state.current_event.description}</p>`
                     `<h2>${this.props.InfoWindow.current_event.name}</h2>
                     <h4>Hosted By: ${this.props.InfoWindow.current_event.group.name}</h4>
                     <h4>When: ${new Date(this.props.InfoWindow.current_event.time)}</h4>
@@ -134,19 +99,24 @@ class MapContainer extends Component {
   }
 }
 
-// MapContainer.propTypes = {
-//   MeetupEvents: PropTypes.arrayof(PropTypes.object)
-// };
-//
-// MapContainer.defaultProps = {
-//   MeetupEvents: 'n/a'
-// };
+MapContainer.propTypes = {
+  MeetupEvents: PropTypes.arrayof(PropTypes.object),
+  toggleInfoWindow: PropTypes.func,
+  closeInfoWindow: PropTypes.func,
+  InfoWindow: PropTypes.objectof(PropTypes.object)
+};
+
+MapContainer.defaultProps = {
+  MeetupEvents: 'n/a',
+  toggleInfoWindow: 'n/a',
+  closeInfoWindow: 'n/a',
+  InfoWindow: 'n/a'
+};
 
 function mapStateToProps(state) {
   return {
     MeetupEvents: state.Meetup.eventResults.results,
     InfoWindow: state.MeetupMap
-    // Location: state.LoggedIn.userData.location
   };
 }
 
