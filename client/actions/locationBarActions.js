ae60980502e6ddbd72d5ddc0407c5d9fe944c598
Kevin Fung <kevinmcfung@gmail.com>
2017-03-17 22:47:51 -0400

    changed api routes to target relative paths

--- client/actions/locationBarActions.js
+++ client/actions/locationBarActions.js
@@ -1,9 +1,7 @@
 import Axios from 'axios';
 
-const ROOT_URL = 'http://localhost:8000/api';
-
 export default function (location) {
-  const request = Axios.get(`${ROOT_URL}/representatives/${location}`);
+  const request = Axios.get(`/api/representatives/${location}`);
   return {
     type: 'Location_Search',
     payload: request
